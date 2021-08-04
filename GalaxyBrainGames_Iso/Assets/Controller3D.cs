255e6dd45dc48848f3659b39009eda7e4dea64fa
Volpanic <volpanicstudios@gmail.com>
2021-08-05 01:50:11 +1000

    Made Controller3D more modular and cleaned and commented it's code. Changed code in player controller for testing new controller.

--- GalaxyBrainGames_Iso/Assets/Controller3D.cs
+++ GalaxyBrainGames_Iso/Assets/Controller3D.cs
@@ -6,53 +6,51 @@ using UnityEngine;
 [SelectionBase]
 public class Controller3D : MonoBehaviour
 {
-    public float movementSpeed;
-    public float gravity = 2.5f;
-    public CapsuleCollider myCollider;
-    public LayerMask groundLayer;
+    [Header("Movement")]
+    [SerializeField, Min(0)] private float gravity = 2.5f;
+    [SerializeField, Min(0)] private float maxGravity = 10f;
 
-    public bool smooth = false;
-    public float smoothSpeed = 10f;
+    [Header("References")]
+    [SerializeField] private CapsuleCollider myCollider;
+    [SerializeField] private LayerMask groundLayer;
+
+    [Header("Smoothing")]
+    [SerializeField] private bool smooth = false;
+    [SerializeField] private float smoothSpeed = 10f;
 
     //Movement
-    private Vector3 velocity;
+    private Vector3 accumulatedVelocity;
     private Vector3 move;
     private RaycastHit groundHit;
     private bool grounded;
     private float currentGravity = 0;
     private Vector3 vel;
 
-    //
-
-    private void Update()
+    public void SimpleMove(Vector3 movement)
     {
-        Gravity();
-        SimpleMove();
+        accumulatedVelocity += movement;
+        ApplyGravity(ref accumulatedVelocity);
         FinalMove();
         GroundCheck();
         CollisionCheck();
     }
 
-    public void SimpleMove()
-    {
-        move = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
-        velocity += move;
-    }
-
     private void FinalMove()
     {
-        Vector3 vel = new Vector3(velocity.x,velocity.y, velocity.z) * movementSpeed;
-        vel = transform.TransformDirection(velocity) * movementSpeed;
+        //Move using the accumulated velocity.
+        Vector3 vel = new Vector3(accumulatedVelocity.x, accumulatedVelocity.y, accumulatedVelocity.z);
 
-        transform.position += vel * Time.deltaTime;
-        velocity = Vector3.zero;
+        transform.position += vel * Time.fixedDeltaTime;
+        accumulatedVelocity = Vector3.zero;
     }
 
-    private void Gravity()
+    //UpdatesGravity
+    private void ApplyGravity(ref Vector3 velocity)
     {
         if(grounded == false)
         {
-            velocity.y -= gravity;
+            //Subtract gravity until max gravity is reached
+            velocity.y = Mathf.Clamp(velocity.y - gravity, -maxGravity, float.MaxValue);
         }
     }
     
@@ -62,6 +60,7 @@ public class Controller3D : MonoBehaviour
         Ray ray = new Ray(myCollider.bounds.center,Vector3.down);
         RaycastHit temp;
 
+        //Raycast down significant distance to do basic ground check.
         if(Physics.SphereCast(ray,myCollider.radius,out temp,20f,groundLayer))
         {
             GroundConfirm(temp);
@@ -72,7 +71,7 @@ public class Controller3D : MonoBehaviour
         }
     }
 
-    private Vector3 groundCheckPoint = new Vector3(0, -0.87f, 0);
+    private Vector3 groundCheckPoint = new Vector3(0, -0.5f, 0);
     private void GroundConfirm(RaycastHit tempHit)
     {
         Collider[] col = new Collider[3];
@@ -87,20 +86,21 @@ public class Controller3D : MonoBehaviour
                 groundHit = tempHit;
                 grounded = true;
 
+                //Snap player to correct Y
                 if(!smooth)
                 {
                     transform.position = new Vector3(transform.position.x,groundHit.point.y + myCollider.bounds.extents.y, transform.position.z);
                 }
                 else
                 {
-                    transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, groundHit.point.y + myCollider.bounds.extents.y, transform.position.z), smoothSpeed * Time.deltaTime);
+                    transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, groundHit.point.y + myCollider.bounds.extents.y, transform.position.z), smoothSpeed * Time.fixedDeltaTime);
                 }
 
                 break;
             }
         }
 
-        if(num <= 1 && tempHit.distance <= myCollider.bounds.size.y * 1.25f)
+        if(num <= 1 && tempHit.distance <= myCollider.bounds.size.y * 1.125f)
         {
             if(col[0] != null)
             {
@@ -130,10 +130,11 @@ public class Controller3D : MonoBehaviour
             Vector3 dir;
             float dist;
 
+            //Get distance into each collider and move opposite of it.
             if(Physics.ComputePenetration(myCollider,transform.position,transform.rotation,overlaps[i],t.position,t.rotation,out dir, out dist))
             {
                 Vector3 penetrationVec = dir * dist;
-                Vector3 velocityProjected = Vector3.Project(velocity,-dir);
+                Vector3 velocityProjected = Vector3.Project(accumulatedVelocity, -dir);
                 transform.position = transform.position + penetrationVec;
                 vel -= velocityProjected;
             }
