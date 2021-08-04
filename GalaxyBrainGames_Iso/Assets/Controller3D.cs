ebd1e825bfe3d85c7e6f2209d62f70593beb17b7
Volpanic <volpanicstudios@gmail.com>
2021-08-04 20:53:42 +1000

    Changed Code in Controller3D to a semi complete charecter controller. added scn_custom_charecter for testing.

--- GalaxyBrainGames_Iso/Assets/Controller3D.cs
+++ GalaxyBrainGames_Iso/Assets/Controller3D.cs
@@ -6,126 +6,137 @@ using UnityEngine;
 [SelectionBase]
 public class Controller3D : MonoBehaviour
 {
-    [Header("Linking")]
-    [SerializeField] private BoxCollider myCollider;
+    public float movementSpeed;
+    public float gravity = 2.5f;
+    public CapsuleCollider myCollider;
+    public LayerMask groundLayer;
 
-    [Header("Toggles")]
-    [SerializeField] private bool useGravity = true;
-    [SerializeField] private bool attachToAnchors;
+    public bool smooth = false;
+    public float smoothSpeed = 10f;
 
-    [Header("Values")]
-    [SerializeField] private float gravityAmount = 9.81f;
+    //Movement
+    private Vector3 velocity;
+    private Vector3 move;
+    private RaycastHit groundHit;
+    private bool grounded;
+    private float currentGravity = 0;
+    private Vector3 vel;
 
-    [Header("Masks")]
-    [SerializeField] private LayerMask groundMask;
-    [SerializeField] private LayerMask anchorMask;
+    //
 
-    private Vector3 velocity = Vector3.zero;
-    private CreatureAnchorPoint anchorToo;
-
-    void FixedUpdate()
+    private void Update()
     {
-        if (!anchorToo)
+        Gravity();
+        SimpleMove();
+        FinalMove();
+        GroundCheck();
+        CollisionCheck();
+    }
+
+    public void SimpleMove()
+    {
+        move = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
+        velocity += move;
+    }
+
+    private void FinalMove()
+    {
+        Vector3 vel = new Vector3(velocity.x,velocity.y, velocity.z) * movementSpeed;
+        vel = transform.TransformDirection(velocity) * movementSpeed;
+
+        transform.position += vel * Time.deltaTime;
+        velocity = Vector3.zero;
+    }
+
+    private void Gravity()
+    {
+        if(grounded == false)
         {
-            if (useGravity && !OnGround()) ApplyGravity();
-
-            Vector2 vel = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-
-            velocity.x = vel.x * 0.2f;
-            velocity.z = vel.y * 0.2f;
-
-            CheckCollisions();
+            velocity.y -= gravity;
         }
+    }
+    
 
-        if (attachToAnchors)
+    private void GroundCheck()
+    {
+        Ray ray = new Ray(myCollider.bounds.center,Vector3.down);
+        RaycastHit temp;
+
+        if(Physics.SphereCast(ray,myCollider.radius,out temp,20f,groundLayer))
         {
-            UpdateAnchorPoint();
-            if(anchorToo != null && velocity != Vector3.zero)
+            GroundConfirm(temp);
+        }
+        else
+        {
+            grounded = false;
+        }
+    }
+
+    private Vector3 groundCheckPoint = new Vector3(0, -0.87f, 0);
+    private void GroundConfirm(RaycastHit tempHit)
+    {
+        Collider[] col = new Collider[3];
+        int num = Physics.OverlapSphereNonAlloc(transform.TransformPoint(groundCheckPoint),0.57f,col,groundLayer);
+
+        grounded = false;
+
+        for(int i = 0; i < num; i++)
+        {
+            if(col[i].transform == tempHit.transform)
             {
-                anchorToo.DetachCurrent();
-                anchorToo = null;
+                groundHit = tempHit;
+                grounded = true;
+
+                if(!smooth)
+                {
+                    transform.position = new Vector3(transform.position.x,groundHit.point.y + myCollider.bounds.extents.y, transform.position.z);
+                }
+                else
+                {
+                    transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, groundHit.point.y + myCollider.bounds.extents.y, transform.position.z), smoothSpeed * Time.deltaTime);
+                }
+
+                break;
             }
         }
-    }
 
-    private bool OnGround()
-    {
-        return Physics.CheckBox(myCollider.bounds.center + (Vector3.down * 0.025f), myCollider.bounds.extents, transform.rotation, groundMask);
-    }
-
-    private void CheckCollisions()
-    {
-        RaycastHit hit;
-        Vector3 center = myCollider.bounds.center;
-        Vector3 extents = myCollider.bounds.extents;
-
-        //Vector3 horizontal = new Vector3(velocity.x, 0, 0);
-        //Vector3 vertical   = new Vector3(0, velocity.y, 0);
-        //Vector3 depth      = new Vector3(0, 0, velocity.z);
-
-        Vector3 positon = transform.position;
-
-        // Horizontal
-        if (Physics.BoxCast(center, extents, new Vector3(Mathf.Sign(velocity.x), 0, 0), out hit, transform.rotation, Mathf.Abs(velocity.x), groundMask))
+        if(num <= 1 && tempHit.distance <= myCollider.bounds.size.y * 1.25f)
         {
-            positon.x = hit.point.x + (Mathf.Sign(hit.normal.x) * extents.x);
-            velocity.x = 0;
-        }
-
-        // Vertical
-        if (Physics.BoxCast(center, extents, new Vector3(0, Mathf.Sign(velocity.y), 0), out hit, transform.rotation, Mathf.Abs(velocity.y), groundMask))
-        {
-            positon.y = hit.point.y + (Mathf.Sign(hit.normal.y) * extents.y);
-            velocity.y = 0;
-        }
-
-        // Depth
-        if (Physics.BoxCast(center, extents, new Vector3(0, 0, Mathf.Sign(velocity.z)), out hit, transform.rotation, Mathf.Abs(velocity.z), groundMask))
-        {
-            positon.z = hit.point.z + (Mathf.Sign(hit.normal.z) * extents.z);
-            velocity.z = 0;
-        }
-
-        transform.position = positon + velocity;
-    }
-
-    public void UpdateAnchorPoint()
-    {
-        if (anchorToo != null) return;
-
-        Collider anchorCollider = CheckForAnchorPoint();
-
-        if (anchorCollider != null)
-        {
-            CreatureAnchorPoint anchor = anchorCollider.gameObject.GetComponent<CreatureAnchorPoint>();
-
-            if (anchor != null)
+            if(col[0] != null)
             {
-                //if (anchor.AttemptAttach(gameObject, new Vector3(0, myCollider.bounds.extents.y, 0)))
-                if (anchor.AttemptAttach(gameObject, new Vector3(0, 0, 0)))
+                Ray ray = new Ray(myCollider.bounds.center, Vector3.down);
+                RaycastHit hit;
+
+                if(Physics.Raycast(ray,out hit, myCollider.bounds.extents.y * 1.25f,groundLayer))
                 {
-                    anchorToo = anchor;
-                    velocity = Vector3.zero;
+                    if(hit.transform != col[0].transform)
+                    {
+                        grounded = false;
+                        return;
+                    }
                 }
             }
-
         }
     }
 
-    private Collider CheckForAnchorPoint()
+    private void CollisionCheck()
     {
-        Collider[] colls = Physics.OverlapBox(myCollider.bounds.center, myCollider.bounds.extents, myCollider.transform.localRotation, anchorMask);
+        Collider[] overlaps = new Collider[4];
+        int num = Physics.OverlapSphereNonAlloc(myCollider.bounds.center,myCollider.radius,overlaps,groundLayer,QueryTriggerInteraction.UseGlobal);
 
-        if (colls != null && colls.Length != 0)
+        for(int i = 0; i < num; i++)
         {
-            return colls[0];
+            Transform t = overlaps[i].transform;
+            Vector3 dir;
+            float dist;
+
+            if(Physics.ComputePenetration(myCollider,transform.position,transform.rotation,overlaps[i],t.position,t.rotation,out dir, out dist))
+            {
+                Vector3 penetrationVec = dir * dist;
+                Vector3 velocityProjected = Vector3.Project(velocity,-dir);
+                transform.position = transform.position + penetrationVec;
+                vel -= velocityProjected;
+            }
         }
-
-        return null;
-    }
-
-    private void ApplyGravity()
-    {
-        velocity += Vector3.down * gravityAmount * Time.fixedDeltaTime;
     }
 }
