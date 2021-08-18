540c8e0105d13a0742fbc1b165ccaa9a1fecb750
Volpanic <volpanicstudios@gmail.com>
2021-08-19 09:03:08 +1000

    Ryan - Added custom read only attribute.

---
+++ GalaxyBrainGames_Iso/Assets/Attributes/ReadOnlyAttribute.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadOnlyAttribute : PropertyAttribute
+{
+    // Custom Attributes need 2 paths, the Property Attribute
+    // and the Property Drawer, This file is empty because 
+    // the attribute doesn't need to hold any data, but
+    // still needs this to exist.
+}
