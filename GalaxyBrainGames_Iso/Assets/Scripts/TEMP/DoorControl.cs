3986d2c9b6ff84e96565e4db4219646b51554715
wbeaven <wtbeaven@gmail.com>
2021-09-10 11:16:25 +1000

    William - Add temporary functionality
    Added door open/close script, pressure plate prefab, lever prefab, on trigger exit script

---
+++ GalaxyBrainGames_Iso/Assets/Scripts/TEMP/DoorControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorControl : MonoBehaviour
+{
+    public GameObject element;
+    public bool isOpen = false;
+    public void Both()
+    {
+        if (isOpen)
+        {
+            element.SetActive(true);
+            isOpen = false;
+        }
+        else
+        {
+            element.SetActive(false);
+            isOpen = true;
+        }
+    }
+}
