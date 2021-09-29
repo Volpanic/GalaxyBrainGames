9f952dcce70b813b221146a6ef4a111274cccb0e
Volpanic <volpanicstudios@gmail.com>
2021-09-29 15:25:30 +1000

    Ryan - Made it so only the child creature can collect a wisp at the end of a level.

--- GalaxyBrainGames_Iso/Assets/Scripts/Triggers/GameEventTrigger.cs
+++ GalaxyBrainGames_Iso/Assets/Scripts/Triggers/GameEventTrigger.cs
@@ -1,3 +1,4 @@
+using GalaxyBrain.Creatures;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,14 +13,33 @@ namespace GalaxyBrain.Interactables
         [SerializeField] private GameEvent onTriggerEnter;
         [SerializeField] private UnityEvent onTriggerEnterEvent;
 
+        [Header("Creature")]
+        [SerializeField] private bool OnlyCreatures = false;
+        [SerializeField] private PlayerController.PlayerTypes creatureType;
+
         private bool activated;
 
         private void OnTriggerEnter(Collider other)
         {
             if (onlyOnce && activated) return;
-            onTriggerEnter?.Raise();
-            onTriggerEnterEvent.Invoke();
-            activated = true;
+
+            if (!OnlyCreatures)
+            {
+                onTriggerEnter?.Raise();
+                onTriggerEnterEvent.Invoke();
+                activated = true;
+            }
+            else
+            {
+                PlayerController controller = other.GetComponent<PlayerController>();
+
+                if(controller != null && controller.PlayerType == creatureType)
+                {
+                    onTriggerEnter?.Raise();
+                    onTriggerEnterEvent.Invoke();
+                    activated = true;
+                }
+            }
         }
     }
 }
