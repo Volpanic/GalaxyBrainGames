8cc4f6b6102886d5cc1cae9699fdbb331d6f2885
Volpanic <volpanicstudios@gmail.com>
2021-10-07 16:47:38 +1100

    Ryan - Added pressure plate script, fixed Vine Block prefab not having the correct push block indecator.

---
+++ GalaxyBrainGames_Iso/Assets/Scripts/Level/PressurePlate.cs
@@ -0,0 +1,79 @@
+using GalaxyBrain.Audio;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace GalaxyBrain.Interactables
+{
+    public class PressurePlate : MonoBehaviour
+    {
+        [Header("Events")]
+        [SerializeField] private GameEvent onPlatePressed;
+        [SerializeField] private GameEvent onPlateReleased;
+
+        [SerializeField] private UnityEvent onPlatePressedEvent;
+        [SerializeField] private UnityEvent onPlateReleasedEvent;
+
+        [Header("Sounds")]
+        [SerializeField] private AudioData onPressedSound;
+        [SerializeField] private AudioData onReleasedSound;
+
+        private Collider collider;
+        private bool pressedDown = false;
+        private int startCollisionCount = 0;
+
+        //Is a layermask with every layer enabled
+        private const int ALL_LAYERS = ~0;
+
+        private void Awake()
+        {
+            collider = GetComponent<Collider>();
+
+            // Most likely 1, we keep track of this because
+            // the pressure plate is most likely in the ground slightly
+            // so this lets us ignore it in the collision check.
+            startCollisionCount = GetCollidersTouching();
+        }
+
+        private int GetCollidersTouching()
+        {
+            return Physics.OverlapBox(collider.bounds.center, collider.bounds.extents, Quaternion.identity, ALL_LAYERS, QueryTriggerInteraction.Ignore).Length;
+        }
+
+        private void FixedUpdate()
+        {
+            if(GetCollidersTouching() > startCollisionCount)
+            {
+                if(!pressedDown)
+                {
+                    onPressedSound?.Play();
+                    onPlatePressed?.Raise();
+                    onPlatePressedEvent.Invoke();
+                    pressedDown = true;
+                }
+            }
+            else
+            {
+                if (pressedDown)
+                {
+                    onReleasedSound?.Play();
+                    onPlateReleased?.Raise();
+                    onPlateReleasedEvent.Invoke();
+                    pressedDown = false;
+                }
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+
+        }
+    }
+}
