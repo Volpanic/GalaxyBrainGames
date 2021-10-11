5634be47d0a32a80f1c72a921a93c1dcc42af4d2
Volpanic <volpanicstudios@gmail.com>
2021-10-11 13:06:54 +1100

    Ryan - Added Wrap and SinWave to number extensions.

--- GalaxyBrainGames_Iso/Assets/Scripts/Utility/Extensions/NumberExtensions.cs
+++ GalaxyBrainGames_Iso/Assets/Scripts/Utility/Extensions/NumberExtensions.cs
@@ -11,6 +11,31 @@ namespace GalaxyBrain.Utility.Extnesion
             return (value - originalMin) / (originalMax - originalMin) * (newMax - newMin) + newMin;
         }
 
+        public static float Wrap(this float value,float min, float max)
+        {
+            float mod = (value - min) % (max - min);
+            if (mod < 0)
+            {
+                return mod + max;
+            }
+            else
+            {
+                return mod + min;
+            }
+        }
+
+        public static float SinWave(this float value,float from, float to, float duration, float offset, float timer)
+        {
+            float normalizedTime = (timer / duration);
+
+            //apply offset
+            normalizedTime = (normalizedTime + offset).Wrap(0f, 1f);
+
+            float sinPosition = Mathf.Sin(normalizedTime * (Mathf.PI*2f));
+
+            return sinPosition.Remap(-1f,1f,from,to);
+        }
+
         public static Vector3 MakeCardinal(this Vector3 direction)
         {
             float absX = Mathf.Abs(direction.x);
