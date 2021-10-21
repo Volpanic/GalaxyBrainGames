e414c817a08d38ac07c30464f33bc1930962f0ed
Volpanic <volpanicstudios@gmail.com>
2021-10-21 16:53:08 +1100

    Ryan - Made Fade.cs block raycasts while fading to prevent buttons from being clicked while transitioning scene, also added summary comment and removed unused namespaces from it.

--- GalaxyBrainGames_Iso/Assets/Scripts/UI/Fade.cs
+++ GalaxyBrainGames_Iso/Assets/Scripts/UI/Fade.cs
@@ -1,17 +1,18 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace GalaxyBrain.UI
 {
+    /// <summary>
+    /// Fades a UI canvas in and out and invokes an event when it's completed
+    /// </summary>
     public class Fade : MonoBehaviour
     {
         //Fade info
         [SerializeField] private CanvasGroup FadeInGroup;
         [SerializeField] private bool fadeIn = true;
 
-        //This action is invoked when a fade is completed, it then clears iteself.
+        //This action is invoked when a fade is completed, it then clears itself.
         private Action afterFade;
 
         public bool FadeDone
@@ -41,11 +42,12 @@ namespace GalaxyBrain.UI
                 FadeInGroup.alpha = Mathf.MoveTowards(FadeInGroup.alpha, 1, Time.unscaledDeltaTime);
                 if (FadeInGroup.alpha == 1) FadeComplete();
             }
-
         }
 
         private void FadeComplete()
         {
+            if (FadeInGroup.alpha == 0) FadeInGroup.blocksRaycasts = false;
+            else FadeInGroup.blocksRaycasts = true;
             afterFade?.Invoke();
             afterFade = null;
             enabled = false;
@@ -54,6 +56,7 @@ namespace GalaxyBrain.UI
         //Actions chain an event to happen when the fade is completed.
         public void FadeIn(Action fadeCompleteEvent = null)
         {
+            FadeInGroup.blocksRaycasts = true;
             FadeInGroup.alpha = 1;
             fadeIn = true;
             enabled = true;
@@ -62,6 +65,7 @@ namespace GalaxyBrain.UI
 
         public void FadeOut(Action fadeCompleteEvent = null)
         {
+            FadeInGroup.blocksRaycasts = true;
             FadeInGroup.alpha = 0;
             fadeIn = false;
             enabled = true;
