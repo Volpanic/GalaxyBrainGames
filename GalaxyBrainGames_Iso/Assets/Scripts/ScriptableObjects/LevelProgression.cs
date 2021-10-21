f51fb827e799d2fb8341ab0b49cf99fd32836da9
Volpanic <volpanicstudios@gmail.com>
2021-10-22 10:17:57 +1100

    Ryan - Added a failsafe to the level progression scriptable object that removes scenes from the progression if they are deleted.

--- GalaxyBrainGames_Iso/Assets/Scripts/ScriptableObjects/LevelProgression.cs
+++ GalaxyBrainGames_Iso/Assets/Scripts/ScriptableObjects/LevelProgression.cs
@@ -1,13 +1,16 @@
 using GalaxyBrain.Attributes;
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace GalaxyBrain.Systems
 {
+    /// <summary>
+    /// A scirptable object that stores the level order of the game.
+    /// Also used to determine what level comes next.
+    /// </summary>
     [CreateAssetMenu]
     public class LevelProgression : ScriptableObject
     {
@@ -17,6 +20,21 @@ namespace GalaxyBrain.Systems
         public GameObject SceneTransition;
 
         private Transform canvasRoot;
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            //If a scene is deleted, remove it from the level progression
+            for(int i = 0; i < ScenesInOrder.Count; i++)
+            {
+                if(AssetDatabase.FindAssets(ScenesInOrder[i]).Length <= 0)
+                {
+                    ScenesInOrder.RemoveAt(i);
+                    i--;
+                }
+            }
+        }
+#endif
 
         private void OnEnable()
         {
