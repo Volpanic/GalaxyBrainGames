d58870cf6aff5feee4783c1805fd762a160056d5
Volpanic <volpanicstudios@gmail.com>
2021-10-21 16:12:34 +1100

    Ryan - Removed unused namespaces and summary comments to audio related scripts.

--- GalaxyBrainGames_Iso/Assets/Scripts/Audio/AudioType.cs
+++ GalaxyBrainGames_Iso/Assets/Scripts/Audio/AudioType.cs
@@ -1,9 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 namespace GalaxyBrain.Audio
 {
+    /// <summary>
+    /// Used by the AudioData scriptable object to determine 
+    /// how to play the sound when Play is called.
+    /// </summary>
     public enum AudioType
     {
         Sound,
