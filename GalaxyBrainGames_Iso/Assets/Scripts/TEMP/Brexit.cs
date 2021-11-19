b1de5bccc4b31f69ec223505400e14648499b39c
William <wtbeaven@gmail.com>
2021-11-19 16:49:05 +1100

    William - Made splashscreen go to menu

---
+++ GalaxyBrainGames_Iso/Assets/Scripts/TEMP/Brexit.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace GalaxyBrain
+{
+    public class Brexit : MonoBehaviour
+    {
+        public void BrexitButton()
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
